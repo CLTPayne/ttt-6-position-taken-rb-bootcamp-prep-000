62aa9e8999723306ebac3a8be0a3e2279eb50808
Charlotte Payne <cltpayne@gmail.com>
2018-03-30 09:33:41 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken? (board, index)
   if board[index] == " " || ""
     return false
-  else board[index] != " " || "" 
+  else board[index] == "X" || "O" 
     return true
   end 
 end
